882aa91aaa29d5f058fc7b399f375acbcb4632f2
merhoo <meredith.hoo1@gmail.com>
2019-01-21 17:14:05 -0500

    played with tree library

--- trees.py
+++ trees.py
@@ -4,3 +4,11 @@ root = Node(1)
 
 child1 = Node(2, parent=root)
 child2 = Node(3, parent=root)
+sam = Node("Sam", parent=child1)
+
+print(root)
+
+for pre, fill, node in RenderTree(root):
+    print("%s%s" % (pre, node.name))
+
+print(root.children)
