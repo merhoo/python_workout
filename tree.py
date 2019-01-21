d06caba0ddf031ab8fd135ca309c54293d6bc116
merhoo <meredith.hoo1@gmail.com>
2019-01-21 17:19:50 -0500

    init tree

---
+++ tree.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self, value=None):
+        self.value = None
+
+
+class Tree:
+    def __init__(self):
+        self.left = None
+        self.right = None
+        self.value = None
