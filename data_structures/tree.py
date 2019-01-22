7179f03d732ea06f0aae50a00cdc0cd2ce716eb3
merhoo <meredith.hoo1@gmail.com>
2019-01-21 23:47:38 -0500

    tree preorder, inorder, postorder traversal

--- data_structures/tree.py
+++ data_structures/tree.py
@@ -30,8 +30,36 @@ class Node:
             return self.left.find(value)
         else:
             return True
+
+    def preorder(self):
+        print self.value,
+        if self.left:
+            self.left.preorder()
+        if self.right:
+            self.right.preorder()
+
+    def inorder(self):
+        if self.left:
+            self.left.inorder()
+        print self.value,
+        if self.right:
+            self.right.inorder()
+
+    def postorder(self):
+        if self.left:
+            self.left.inorder()
+        if self.right:
+            self.right.inorder()
+        print self.value,
+
 root = Node(5)
 root.add(4)
 root.add(3)
 root.add(9)
 print(root.find(4))
+root.preorder()
+print
+root.postorder()
+print
+root.inorder()
+print
